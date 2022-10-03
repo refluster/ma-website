33021dc7d7d50c5e461ffff13e9f4d5945fe66a7
Koh Uehara <refluster@gmail.com>
2022-10-03 15:21:30 -0700

    react based

--- ma-website/src/App.tsx
+++ ma-website/src/App.tsx
@@ -1,24 +1,17 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { customTheme } from './styles/theme';
+import Index from './pages/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={customTheme}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ ma-website/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+
+export default function Index () {
+    return (
+        <Box>Here is index.</Box>
+    )
+};
+
